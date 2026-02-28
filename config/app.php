e87d17dd9928a7984bae4088acb3bb6a198f81a2
unknown <wwwronyraihan123@gmail.com>
2026-02-28 06:08:03 +0600

    success redirect url given

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
-    'frontend_url' => env('FRONTEND_URL', 'https://property-hub-rho.vercel.app'),
+    'frontend_url' => env('FRONTEND_URL', 'https://property-hub-rho.vercel.app/success'),
 
     /*
     |--------------------------------------------------------------------------
